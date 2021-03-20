207fbe27803ccb66df3baf4b9d2150fd6222e273
m1github <mahesh_kumar1019@yahoo.in>
2021-03-20 09:36:25 +0530

    socket connection completed

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express();
+const socketio = require('socket.io');
 
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
+//Run when client connects
+io.on('connection',socket => {
+    console.log('New WS connection...');
+
+    socket.emit('msg','Welcome to heyApp created by Mahesh!')
+});
 const port = process.env.PORT || 4000;
-app.listen(port,()=>console.log(`App started on port: ${port}`));
+server.listen(port,()=>console.log(`App started on port: ${port}`));
