b583cc3ea5c44ae4cec0eab5128cc434d3f7620e
m1github <mahesh_kumar1019@yahoo.in>
2021-03-20 17:20:45 +0530

    username emiting when leaving chat room

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const messageFormat = require('./util/messages')
+const {userJoin,getCurrentUser,userLeave,getRoomUsers} = require('./util/users')
 
 
 const app = express();
@@ -15,25 +16,34 @@ app.use(express.static(path.join(__dirname,'public')));
 const botname = 'heyApp Admin';
 
 //Run when client connects
-io.on('connection',socket => {
-    //There are three ways of emiting msg
+//There are three ways of emiting msg
     //1.    socket.emit() --> to the user
     //2.    socket.broadcast.emit() --> to everyone except for the user
     //3.    io.emit() --> all the client in general
-    socket.emit('message',messageFormat(botname,'Welcome to heyApp chatting platform.'));
+io.on('connection',socket => {
+    socket.on('joinRoom',({username,room})=>{
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room)
+        //Welcome msg when a user joins chat
+        socket.emit('message',messageFormat(botname,'Welcome to heyApp chatting platform.'));
 
-    socket.broadcast.emit('message',messageFormat(botname,'A user has joined:'));
+        socket.broadcast.to(user.room).emit('message',messageFormat(botname,`${user.username} has joined the chat room.`));
+    }); 
+    
+    //listen for chatMessage
+    socket.on('chatMessage',msg=>{
+        const user = getCurrentUser(socket.id);
+        console.log(msg);
+        io.to(user.room).emit('message',messageFormat(user.username,msg));
+    });
 
     //Runs when a user disconnects
     socket.on('disconnect',()=>{
-        io.emit('message',messageFormat(botname,'A user has left the chat.'));
+        const user = userLeave(socket.id);
+        if(user){
+            io.to(user.room).emit('message',messageFormat(botname,`${user.username} has left the chat.`));
+        }
     });
-
-    //listen for chatMessage
-    socket.on('chatMessage',msg=>{
-        console.log(msg);
-        io.emit('message',messageFormat('User',msg));
-    })
 });
 const port = process.env.PORT || 4000;
 server.listen(port,()=>console.log(`App started on port: ${port}`));
